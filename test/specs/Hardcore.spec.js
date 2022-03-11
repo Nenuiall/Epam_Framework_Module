62cfbc8e6183d1a73455f2f7dc05e8166fc26686
Nenuiall <95130542+Nenuiall@users.noreply.github.com>
2022-03-11 17:33:56 +0300

    framework

--- test/specs/Hardcore.spec.js
+++ test/specs/Hardcore.spec.js
@@ -1,29 +1,17 @@
-/*При выполнении задания необходимо использовать возможности Selenium WebDriver, 
-юнит-тест фреймворка и концепцию Page Object. Автоматизировать следующий сценарий:
-1. Открыть https://cloud.google.com/ 
-2. Нажав кнопку поиска по порталу вверху страницы, ввести в поле поиска"Google Cloud Platform Pricing Calculator"
-3. Запустить поиск, нажав кнопку поиска.
-4. В результатах поиска кликнуть "Google Cloud Platform Pricing Calculator" и перейти на страницу калькулятора.
-5. Активировать раздел COMPUTE ENGINE вверху страницы
-6. Заполнить форму следующими данными:
-    * Number of instances: 4
-    * What are these instances for?: оставить пустым
-    * Operating System / Software: Free: Debian, CentOS, CoreOS, Ubuntu, or other User Provided OS
-    * VM Class: Regular
-    * Instance type: n1-standard-8    (vCPUs: 8, RAM: 30 GB)
-    * Выбрать Add GPUs
-        * Number of GPUs: 1
-        * GPU type: NVIDIA Tesla V100
-    * Local SSD: 2x375 Gb
-    * Datacenter location: Frankfurt (europe-west3)
-    * Commited usage: 1 Year
-7. Нажать Add to Estimate
-8. Выбрать пункт EMAIL ESTIMATE
-9. В новой вкладке открыть https://yopmail.com/ или аналогичный сервис для генерации временных email'ов
-10. Скопировать почтовый адрес сгенерированный в yopmail.com
-11. Вернуться в калькулятор, в поле Email ввести адрес из предыдущего пункта
-12. Нажать SEND EMAIL
-13. Дождаться письма с рассчетом стоимости и проверить что Total Estimated Monthly Cost в письме совпадает с тем, что отображается в калькуляторе*/
+/*Построить фреймворк для автоматизации задания Hardcore из 
+eLearn курса WebDriver.
+
+Что должно быть в итоговом фреймворке: 
+
+- WebDriverIO для управления коннекторам к браузерам
+- PageObject/ Page Factory для абстракций страниц
+- Модель для бизнес-объектов необходимых сущностей
+- Файлы с тестовыми данными для разных окружений (как минимум 2)
+- Suites для smoke тестов и всех типов тестов 
+- Добавить spec-reporter к существующим автотестам
+- При падении теста должен быть сделан скриншот с датой и временем 
+- Фреймворк должен иметь возможность запуска с Jenkins и параметризацией браузера, тест suite, environment. 
+- Результаты тестов должны быть на графике джобы, скриншоты должны быть заархивированы как артефакты*/
 
 
 const GoogleCloudPage =  require('../../pages/googleCloud.page');
